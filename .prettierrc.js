3af2db83483db33993b8fa0ccb478bdcbd19c9fe
orchsik <orchsik@naver.com>
2022-12-01 09:08:55 +0900

    feature prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  jsxBracketSameLine: false,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: "auto",
+};
