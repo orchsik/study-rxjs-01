6b36bae3a75429de190102fd268df8ae1865f1ea
orchsik <orchsik@naver.com>
2022-12-04 17:13:14 +0900

    feature; tamp

--- 10-timer_interval/index.ts
+++ 10-timer_interval/index.ts
@@ -2,27 +2,27 @@ import { Observable, Observer, timer, interval } from "rxjs";
 
 console.log("App started");
 
-// const subscription = timer(2000).subscribe({
-//   next: (value) => console.log(value),
-//   complete: () => console.log("Completed"),
-// });
-
-const timer$ = new Observable((subscriber) => {
-  const timeoutId = setTimeout(() => {
-    console.log("SetTimeout");
-    subscriber.next(0);
-    subscriber.complete();
-  }, 2_000);
-
-  return () => {
-    clearTimeout(timeoutId);
-  };
-});
-
-const subscription = timer$.subscribe({
+const subscription = timer(2000).subscribe({
   next: (value) => console.log(value),
   complete: () => console.log("Completed"),
 });
+
+// const timer$ = new Observable((subscriber) => {
+//   const timeoutId = setTimeout(() => {
+//     console.log("SetTimeout");
+//     subscriber.next(0);
+//     subscriber.complete();
+//   }, 2_000);
+
+//   return () => {
+//     clearTimeout(timeoutId);
+//   };
+// });
+
+// const subscription = timer$.subscribe({
+//   next: (value) => console.log(value),
+//   complete: () => console.log("Completed"),
+// });
 
 setTimeout(() => {
   console.log("Unsubscribe");
